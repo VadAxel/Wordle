aba0377293e2979b4b783bd105ef1542c517c667
VadAxel <58472614+VadAxel@users.noreply.github.com>
2022-05-11 12:06:05 +0200

    Co-authored-by: Nomis-N <Nomis-N@users.noreply.github.com>

--- wordleGUI.py
+++ wordleGUI.py
@@ -2,25 +2,16 @@ import PySimpleGUI as sg
 import SwedishWordle
 from file_score_upd import file_score_upd
 from open_hiscore import open_hiscore
-from theme_change import main_theme,layout
-#from score_output import score_output
 
-#from word_eva import answer_list_converter,guess_split   #high score, snyggt uppdelat för A/L
+#high score, snyggt uppdelat för A/L
 
 game = SwedishWordle.Game(5) # skapa ett nytt wordlespel med ord som är 5 långa
 
-main_theme()
-
-"""
-theme = 'Dark Blue'
-sg.theme(theme)
-"""
-
 def TextChar(value, key):
-    return sg.Input(value, key=key, font='Courier 22', size=(10,100),  disabled_readonly_background_color='gray', border_width=5,  p=1, enable_events=True, disabled=True)
+    return sg.Input(value, key=key, font='Courier 22', size=(10,100), border_width=5,  p=1, enable_events=True, disabled=True)
 
 layout = [
-    [sg.Text("Wooordle", font='_21')], #sg.Text('Teman: ', font='_19'), sg.B('Dark Purple', key='darkpurple6_button'), sg.B('Light Blue', key='lightblue_button'), sg.B('Bright Colors', key='brightcolors_button'), sg.B('Dark Blue', key='darkblue')], 
+    [sg.Text("Wooordle", font='_21')],  
     [sg.HorizontalSeparator(color='black')],
     [sg.Text('', key='string1')],
     [sg.HorizontalSeparator(color='black')],
@@ -42,74 +33,94 @@ layout = [
     [sg.Text('\n\n\n', font='_ 10', key = 'High_Score')], 
     ]
 
-window = sg.Window("Wordle SE", layout, finalize=True)
 
 #Definierar top_3 mha funktionen
 top_3 = open_hiscore() 
 
-#uppdaterar grafiken
-def score_update():
+def make_window_theme(theme=None):
+    if theme:
+        score = 0 #Score på varje enskilt game
+        i = 1 #Ta bort, kanske använda "game"/L
+        sg.theme(theme)
+        window = sg.Window("Wordle SE", layout, finalize=True)
+        while True:
+            event, values = window.read()
+            guess = values['input_box']
+            """
+            if event == "darkpurple6_button":
+                main_theme()
+
+            elif event == "lightblue_button":
+                main_theme()
+            
+            elif event == "brightcolors_button":
+                main_theme()
+
+            elif event == "darkblue":
+                main_theme()
+            """
+
+            #Om ogiltigt ord
+            if len(guess) != 5 and i <=5 and event == "confirm_button":
+                window['string'+str(i)].update("Felaktig längd på ord. Du gissade " + guess + ". Detta spel är om ord som är 5 i längd")
+                continue    
+            
+            #Vid korrekt svar
+            elif sum(game.Guess(guess)) == 0:
+                window['string'+str(6)].update("Knasvinst län")
+                score_update_visual(score, window)      
+            
+
+            elif sum(game.Guess(guess)) != 0 and event == sg.WIN_CLOSED:
+                #värdet 99 ges till den som inte klara spelet, topplista är för de som lyckas
+                score = 99
+                break
+            
+            #Vanlig gissning
+            elif event == "confirm_button" and i <= 5:           
+                
+                #Om gränsen för antalet gissningar överskrids, förlorar du
+                if i == 5:
+                    window['string'+str(6)].update("Choktorsk bram")
+                    score_update_visual(score, window)
+                
+                #Grafisk validering av gissade ordet
+                guess_split = guess.split()  #Skall bli funktion
+                score = score + sum(game.Guess(guess))
+                window['string'+str(i)].update((game.Guess(guess),guess_split))
+                score_update_visual(score, window)
+                i += 1
+            #Om man stänger wordle efter vinst
+            elif event == sg.WIN_CLOSED:
+                break
+
+
+            file_score_upd(score)
+    layout2 = [[sg.T('Denna ruta representerar valt tema')],
+              [sg.Button('Ok'), sg.Button('Ändra tema'), sg.Button('Avsluta')]]
+
+    return sg.Window('Wordle Wizard', layout2)
+
+def score_update_visual(score, window):
     window['yiscore'].update(score)
     window['High_Score'].update((top_3))
 
 
+def main_theme():
+    window_theme = make_window_theme()
 
-score = 0 #Score på varje enskilt game
-i = 1 #Ta bort, kanske använda "game"/L
+    while True:
+        event, values = window_theme.read()
+        if event == sg.WINDOW_CLOSED or event == 'Exit':
+            break
+        if event == 'Ändra tema':
+            event, values = sg.Window('Ändra tema',
+                                      [[sg.Combo(sg.theme_list(), readonly=True, k='-THEME LIST-'), sg.OK(), sg.Cancel()]]
+                                      ).read(close=True)
+            if event == 'OK':
+                window_theme.close()
+                window = make_window_theme(values['-THEME LIST-'])
 
-while True:
-    event, values = window.read()
-    guess = values['input_box']
-    """
-    if event == "darkpurple6_button":
-        main_theme()
+    window.close()
 
-    elif event == "lightblue_button":
-        main_theme()
-    
-    elif event == "brightcolors_button":
-        main_theme()
-
-    elif event == "darkblue":
-        main_theme()
-    """
-
-    #Om ogiltigt ord
-    if len(guess) != 5 and i <=5 and event == "confirm_button":
-        window['string'+str(i)].update("Felaktig längd på ord. Du gissade " + guess + ". Detta spel är om ord som är 5 i längd")
-        continue    
-    
-    #Vid korrekt svar
-    elif sum(game.Guess(guess)) == 0:
-        window['string'+str(6)].update("Knasvinst län")
-        score_update()
-    
-
-    elif sum(game.Guess(guess)) != 0 and event == sg.WIN_CLOSED:
-        #värdet 99 ges till den som inte klara spelet, topplista är för de som lyckas
-        score = 99
-        break
-    
-    #Vanlig gissning
-    elif event == "confirm_button" and i <= 5:           
-        
-        #Om gränsen för antalet gissningar överskrids, förlorar du
-        if i == 5:
-            window['string'+str(6)].update("Choktorsk bram")
-            score_update()
-        
-        #Grafisk validering av gissade ordet
-        guess_split = guess.split()  #Skall bli funktion
-        score = score + sum(game.Guess(guess))
-        window['string'+str(i)].update((game.Guess(guess),guess_split))
-        score_update()
-        i += 1
-    #Om man stänger wordle efter vinst
-    elif event == sg.WIN_CLOSED:
-        break
-
-
-    file_score_upd(score)
-
-
-window.close()
+main_theme()
