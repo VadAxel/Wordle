476df61ae5810b3f6cbd7d857b6b89a02b30c280
VadAxel <58472614+VadAxel@users.noreply.github.com>
2022-05-11 15:50:00 +0200

    Kommentarer kod

--- wordleGUI.py
+++ wordleGUI.py
@@ -10,7 +10,7 @@ from layout import layout2_func
 #skapa ett nytt wordlespel med ord som är 5 långa
 game = SwedishWordle.Game(5)
 
-
+#Definierar grundläggande grafik
 def TextChar(value, key):
     return sg.Input(value, key=key, font='Courier 22', size=(10,100), border_width=5,  p=1, enable_events=True, disabled=True)
 
@@ -20,9 +20,10 @@ layout = layout_func()
 #Definierar top_3 mha funktionen
 top_3 = open_hiscore() 
 
+#Funktion för uppdatering av score, för kompression av kod
 def score_update_visual(score, window):
-    window['yiscore'].update(score)
-    window['High_Score'].update((top_3))
+    window['yiscore'].update(score)                                                #Aktuellt score
+    window['High_Score'].update((top_3))                                           #High Score
 
 def make_window_theme(theme=None):
     if theme:
@@ -32,7 +33,7 @@ def make_window_theme(theme=None):
         window = sg.Window("Wordle SE", layout, finalize=True)
         while True:
             event, values = window.read()
-            guess = values['input_box']
+            guess = values['input_box']                                            #Input från användare får en variabel
 
                 #Om ogiltigt ord
             if len(guess) != 5 and i <=5 and event == "confirm_button":
@@ -40,31 +41,31 @@ def make_window_theme(theme=None):
                 continue 
               
                 #Vid korrekt svar
-            elif sum(game.Guess(guess)) == 0:
-                window['string'+str(6)].update("Knasvinst län")
-                score_update_visual(score, window)      
-
+            elif sum(game.Guess(guess)) == 0:                
+                window['string'+str(6)].update("Knasvinst län")                    #Grafik meddelande vinst
+                score_update_visual(score, window)                                 #Grafik uppdateas för score      
+                                    
                 #om programmet klickas utan att ha vunnit
             elif sum(game.Guess(guess)) != 0 and event == sg.WIN_CLOSED:
-                    #värdet 99 ges till den som inte klara spelet, topplista är för de som lyckas
+                #värdet 99 ges till den som inte klara spelet, topplista är för de som lyckas
                 score = 99
                 break
                 
-                #Vanlig gissning
+            #Vanlig gissning
             elif event == "confirm_button" and i <= 5:           
-                    #Om gränsen för antalet gissningar överskrids, förlorar du
+                #Om gränsen för antalet gissningar överskrids, förlorar du
                 if i == 5:
                     window['string'+str(6)].update("Choktorsk bram")
-                    score_update_visual(score, window)
-                    
-                    #Grafisk validering av gissade ordet
-                guess_split = guess.split()  #Skall bli funktion
-                score = score + sum(game.Guess(guess))
-                window['string'+str(i)].update((game.Guess(guess),guess_split))
-                score_update_visual(score, window)
+                    score_update_visual(score, window)                              #Grafik uppdateas för score
+
+                #Grafisk validering av gissade ordet
+                guess_split = guess.split()                                         #Skall bli funktion
+                score = score + sum(game.Guess(guess))                              #Score uppdateras
+                window['string'+str(i)].update((game.Guess(guess),guess_split))     #Grafik uppdateras för svar
+                score_update_visual(score, window)                                  #Grafik uppdateas för score
                 i += 1
                 
-                #Om man stänger wordle efter vinst
+            #Om man stänger wordle efter vinst
             elif event == sg.WIN_CLOSED:
                 break
 
@@ -75,9 +76,13 @@ def make_window_theme(theme=None):
 
         return sg.Window('Wordle Wizard', layout2)
 
+#Hanterar skapandet av fönster
 def main_theme():
+    
+    #Startar make_window_theme funktionen
     window_theme = make_window_theme()
 
+    #Funktioner för olika knapptryck för wordle wizard
     while True:
         event, values = window_theme.read()
         if event == sg.WINDOW_CLOSED or event == 'Exit':
@@ -86,11 +91,13 @@ def main_theme():
             event, values = sg.Window('Ändra tema',
                                       [[sg.Combo(sg.theme_list(), readonly=True, k='-THEME LIST-'), sg.OK(), sg.Cancel()]]
                                       ).read(close=True)
+            #Startar make_window_theme funktionen med valt tema
             if event == 'OK':
                 window_theme.close()
                 window = make_window_theme(values['-THEME LIST-'])
                 
 
     window.close()
+
 
 main_theme()
